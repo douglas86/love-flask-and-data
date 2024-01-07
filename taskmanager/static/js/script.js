c7395babfa7ce23e5386421bfd7e4d035b8fc35a
douglas <douglasmaxton@gmail.com>
2024-01-07 20:11:34 +0000

    users can add tasks to db

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.modal');
     M.Modal.init(sidenav);
+
+    // datepicker initialization
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selection = document.querySelectorAll('select');
+    M.FormSelect.init(selection);
 });
