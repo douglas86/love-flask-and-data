9cf400963add03abc9b2eb39be5848cd6219a931
douglas <douglasmaxton@gmail.com>
2024-01-07 18:31:14 +0000

    opens modal on delete btn click from card

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+    let sidenav = document.querySelectorAll('.modal');
+    M.Modal.init(sidenav);
 });
